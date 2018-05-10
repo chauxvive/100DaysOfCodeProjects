696f30b26d73ffcca80fba9ac79d6b307eabae2e
Mary Paul <mpauldesigns@gmail.com>
2018-05-10 12:04:20 -0400

    Added Mario_more to CS50

---
+++ CS50 problems/pset1/mario_more.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //int number;
+    printf("   ##\n  ###\n ####\n#####\n");
+    printf("Would you like to make a Mario pyramid too? \n");
+    printf("How many steps? (<24) \n");
+
+    //get an integer. Reprompt if invalid
+
+    int height;
+    do {
+        height = get_int();
+    }
+    while (height<0 || height>24);
+
+
+// print the pyramid
+    for (int i = 0; i<height; i++){
+        if (height==1){
+            printf("#  #\n");}
+        else if (height==2){
+            i = 1;
+            printf(" #  #\n##  ##\n");}
+
+        else {
+        for (int j = height-i; j>1;j--){
+            printf(" ");
+        }
+        for (int k=0; k<i+2; k++){
+            printf("#");
+        }
+        printf("  ");
+        for (int k=0; k<i+2; k++){
+            printf("#");
+        }
+
+        printf("\n"); }
+    }
+
