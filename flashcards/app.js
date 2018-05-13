bb44b7d07b3ce1c296ed1f22498d70c4ad6972fa
Mary Paul <mpauldesigns@gmail.com>
2018-05-12 22:53:54 -0400

    updated flashcard app

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,2 +1,17 @@
 const express = require("express");
 
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/hello', (req, res) => {
+    res.send("<h1>This is a different page, friend</h1>");
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000')
+});
