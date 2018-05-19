ec918b54bb60103271c57357a234b95cd41e62c5
Mary Paul <mpauldesigns@gmail.com>
2018-05-19 19:49:52 -0400

    Day 10 update

--- JS authentication with express/routes/index.js
+++ JS authentication with express/routes/index.js
@@ -3,6 +3,30 @@ var router = express.Router();
 var User = require('../models/user');
 
 // Login route
+router.get('/login', function(req, res,next){
+  return res.render('login', { title: 'Log In' })
+});
+
+//Login post route
+router.post('/login', function(req, res, next) {
+//  return res.send('Logged In!');
+  if (req.body.email && req.body.password){
+    User.authenticate(req.body.email, req.body.password, function(error, user){
+      if (error || !user){
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
 
 // GET /register
 router.get('/register', function(req, res, next) {
@@ -37,6 +61,7 @@ router.post('/register', function(req, res, next) {
         if (error) {
           return next(error);
         } else {
+          req.session.userId = user._id;
           return res.redirect('/profile');
         }
       });
@@ -65,3 +90,21 @@ router.get('/contact', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+// GET /profile
+router.get('/profile', function(req, res, next) {
+  if (! req.session.userId ) {
+    var err = new Error("You are not authorized to view this page.");
+    err.status = 403;
+    return next(err);
+  }
+  User.findById(req.session.userId)
+      .exec(function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.render('profile', { title: 'Profile', name: user.name, favorite: user.favoriteBook });
+        }
+      });
+});
