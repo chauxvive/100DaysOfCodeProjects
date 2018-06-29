e575647de74e8cc5326a83631e5870912f34104f
Mary Paul <mpauldesigns@gmail.com>
2018-06-29 14:28:26 -0400

    day 40 update

---
+++ FreeCodeCamp/sortAlphabetically.js
@@ -0,0 +1,14 @@
+function alphabeticalOrder(arr) {
+  // Add your code below this line
+  return arr.sort();
+  
+  // Add your code above this line
+}
+console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
+
+
+
+
+//var fruits = ["Banana", "Orange", "Apple", "Mango"];
+//fruits.sort();
+//fruits.reverse();
