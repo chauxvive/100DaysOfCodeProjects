5dbaeb7bd45b950c2a9d9d97cf460be2e4ec87a0
Mary Paul <mpauldesigns@gmail.com>
2018-09-22 12:37:48 -0400

    "Day 5 commit 1 20/48 React"

---
+++ FreeCodeCamp/React/20_Review_using_props_with_stateless_functional_components.js
@@ -0,0 +1,15 @@
+class CampSite extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return ( <
+            div >
+            <
+            Camper / >
+            <
+            /div>
+        );
+    }
+};
+// change code below this line
