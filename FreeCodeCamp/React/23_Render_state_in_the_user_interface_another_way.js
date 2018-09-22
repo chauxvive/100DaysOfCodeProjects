31470a17541b832bec1cb8a642136cfeefd0ee14
Mary Paul <mpauldesigns@gmail.com>
2018-09-22 15:54:50 -0400

    "24/48 react"

--- FreeCodeCamp/React/23_Render_state_in_the_user_interface_another_way.js
+++ FreeCodeCamp/React/23_Render_state_in_the_user_interface_another_way.js
@@ -6,11 +6,13 @@ class MyComponent extends React.Component {
         }
     }
     render() {
+        // change code below this line
+
+        // change code above this line
         return ( <
-            div > { /* change code below this line */ } <
-            h1 > {
-                this.state.name
-            } < /h1> { /* change code above this line */ } <
+            div > { /* change code below this line */ }
+
+            { /* change code above this line */ } <
             /div>
         );
     }
