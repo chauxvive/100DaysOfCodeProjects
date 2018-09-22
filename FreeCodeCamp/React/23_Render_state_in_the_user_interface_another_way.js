9897c9dabe1e137cce82e00c25b8929e1e960408
Mary Paul <mpauldesigns@gmail.com>
2018-09-22 15:57:07 -0400

    23/48 react

--- FreeCodeCamp/React/23_Render_state_in_the_user_interface_another_way.js
+++ FreeCodeCamp/React/23_Render_state_in_the_user_interface_another_way.js
@@ -7,12 +7,13 @@ class MyComponent extends React.Component {
     }
     render() {
         // change code below this line
-
+        const name = this.state.name;
         // change code above this line
         return ( <
-            div > { /* change code below this line */ }
-
-            { /* change code above this line */ } <
+            div > { /* change code below this line */ } <
+            h1 > {
+                name
+            } < /h1> { /* change code above this line */ } <
             /div>
         );
     }
