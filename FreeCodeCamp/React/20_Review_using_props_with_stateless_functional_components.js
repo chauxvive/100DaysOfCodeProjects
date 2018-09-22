dd0cd85ea9e450b360ac93f6efbd72c274d5456e
Mary Paul <mpauldesigns@gmail.com>
2018-09-22 12:41:07 -0400

    day 5 update 2

--- FreeCodeCamp/React/20_Review_using_props_with_stateless_functional_components.js
+++ FreeCodeCamp/React/20_Review_using_props_with_stateless_functional_components.js
@@ -13,3 +13,22 @@ class CampSite extends React.Component {
     }
 };
 // change code below this line
+
+class Camper extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return ( <
+            p > {
+                this.props.name
+            } < /p>
+        )
+    }
+}
+Camper.propTypes = {
+    name: PropTypes.string.isRequired
+};
+Camper.defaultProps = {
+    name: "CamperBot"
+};
