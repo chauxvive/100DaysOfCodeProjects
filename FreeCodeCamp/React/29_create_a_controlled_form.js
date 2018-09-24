29c93809b35c177a73a574f710aedd61b5361538
Mary Paul <mpauldesigns@gmail.com>
2018-09-23 21:37:07 -0400

    day 7 update 2

--- FreeCodeCamp/React/29_create_a_controlled_form.js
+++ FreeCodeCamp/React/29_create_a_controlled_form.js
@@ -13,24 +13,36 @@ class MyForm extends React.Component {
             input: event.target.value
         });
     }
+
     handleSubmit(event) {
         // change code below this line
-
+        this.setState({
+            submit: this.state.input
+        });
+        event.preventDefault();
         // change code above this line
     }
+
+
     render() {
         return ( <
             div >
             <
             form onSubmit = {
                 this.handleSubmit
-            } > { /* change code below this line */ }
-
-            { /* change code above this line */ } <
+            } > { /* change code below this line */ } <
+            input value = {
+                this.state.input
+            }
+            onChange = {
+                this.handleChange
+            }
+            /> { /* change code above this line */ } <
             button type = 'submit' > Submit! < /button> <
-            /form> { /* change code below this line */ }
-
-            { /* change code above this line */ } <
+            /form> { /* change code below this line */ } <
+            h1 > {
+                this.state.submit
+            } < /h1> { /* change code above this line */ } <
             /div>
         );
     }
