4ef763364deb65385a0f6c4d709943ebcb184762
Mary Paul <mpauldesigns@gmail.com>
2018-09-23 22:12:47 -0400

    day 8 update 1

---
+++ FreeCodeCamp/React/32_Use_Lifecycle_Method_componentWillMount.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentWillMount() {
+        // change code below this line
+        console.log("This is a test");
+        // change code above this line
+    }
+    render() {
+        return <div / >
+    }
+};
