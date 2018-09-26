63cc7c34044a90229b673bb30c67d63591f9037f
Mary Paul <mpauldesigns@gmail.com>
2018-09-26 16:31:04 -0400

    44/48 complete

--- FreeCodeCamp/React/38_Add_inline_styles.js
+++ FreeCodeCamp/React/38_Add_inline_styles.js
@@ -1,14 +1,15 @@
-// const styles =
+const styles = {
+    color: "purple",
+    fontSize: 40,
+    border: "2px solid purple"
+}
 // change code above this line
 class Colorful extends React.Component {
     render() {
         // change code below this line
         return ( <
             div style = {
-                {
-                    color: "yellow",
-                    fontSize: 24
-                }
+                styles
             } > Style Me! < /div>
         );
         // change code above this line
