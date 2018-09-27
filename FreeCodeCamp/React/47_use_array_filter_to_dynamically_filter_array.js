32c2dbc1fed1cda6ceb4b1a2ead4333fb6a8ed03
Mary Paul <mpauldesigns@gmail.com>
2018-09-26 23:42:13 -0400

    47 complete

--- FreeCodeCamp/React/47_use_array_filter_to_dynamically_filter_array.js
+++ FreeCodeCamp/React/47_use_array_filter_to_dynamically_filter_array.js
@@ -1,46 +1,55 @@
 class MyComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [{
-                    username: 'Jeff',
-                    online: true
-                },
-                {
-                    username: 'Alan',
-                    online: false
-                },
-                {
-                    username: 'Mary',
-                    online: true
-                },
-                {
-                    username: 'Jim',
-                    online: false
-                },
-                {
-                    username: 'Sara',
-                    online: true
-                },
-                {
-                    username: 'Laura',
-                    online: true
-                }
-            ]
+        constructor(props) {
+            super(props);
+            this.state = {
+                users: [{
+                        username: 'Jeff',
+                        online: true
+                    },
+                    {
+                        username: 'Alan',
+                        online: false
+                    },
+                    {
+                        username: 'Mary',
+                        online: true
+                    },
+                    {
+                        username: 'Jim',
+                        online: false
+                    },
+                    {
+                        username: 'Sara',
+                        online: true
+                    },
+                    {
+                        username: 'Laura',
+                        online: true
+                    }
+                ]
+            }
         }
-    }
-    render() {
-        const usersOnline = null; // change code here
-        const renderOnline = null; // change code here
-        return ( <
-            div >
-            <
-            h1 > Current Online Users: < /h1> <
-            ul > {
-                renderOnline
-            } <
-            /ul> <
-            /div>
-        );
-    }
-};
+        render() {
+            const usersOnline = this.state.users.filter(user => user.online);
+            const renderOnline = usersOnline.map((item) =>
+                <
+                li key = {
+                    item.username
+                } > {
+                    item.username
+                } < /li>)
+
+
+
+                return ( <
+                    div >
+                    <
+                    h1 > Current Online Users: < /h1> <
+                    ul > {
+                        renderOnline
+                    } <
+                    /ul> <
+                    /div>
+                );
+            }
+        };
