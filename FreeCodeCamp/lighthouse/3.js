96209c9bb04cc19508ca58566db1e3da13f6eda5
Mary Paul <mpauldesigns@gmail.com>
2018-10-04 17:15:38 -0400

    day 17 update 3

--- FreeCodeCamp/lighthouse/3.js
+++ FreeCodeCamp/lighthouse/3.js
@@ -1,28 +1,24 @@
+//short word: this exercise made me angry. it was NOT helpful about testing or error messages. Very frustrating.
+
+
+function countRows() {
+    return GRID.length
+}
+
+function countColumns() {
+    return GRID[0].length
+}
+
+
 function gridSize() {
-    const y = GRID.length;
-    console.log(y);
-    const x = GRID[0].length;
-    const sandwich = `${y} + ${x}`;
-    console.log(typeof sandwich);
-    return sandwich;
+    return countColumns().toString() + " x " + countRows().toString();
 }
 
-function gridSize2() {
-    return `${GRID[0].length} x ${GRID.length};`
-}
 
- const GRID = [
-     ["", "", "", "^", "", "", "", "", "", ""],
-     ["", "", "v", "", "~", "", "", "", "", ""],
-     ["", "v", "", "", "^", "^", "", "", "", ""],
-     ["", "", "", "", "^", "^", "", "", "", ""],
-     ["", "", "", "", "", "", "", "", "v", ""],
-     ["", "", "", "", "", "", "", "", "", ""],
-     ["", "", "", "", "", "", "", "", "", ""],
-     ["", "^", "~", "~", "", "", "", "^", "", ""],
-     ["", "^", "", "~", "~", "", "", "", "", ""],
-     ["", "^", "", "", "~", "~", "", "", "", ""],
- ];
-
-
- console.log(gridSize2(GRID));
+//function gridSize() {
+//    let columns = countColumns().toString;
+//    let rows = countRows().toString;
+//    let result = rows + " x " + columns;
+//    return result
+//return `${countColumns(GRID)} x ${countRows(GRID)};`
+//}
