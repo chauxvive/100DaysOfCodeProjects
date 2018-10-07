616dbcff65809be9c434f756f76df76b2d2904dd
Mary Paul <mpauldesigns@gmail.com>
2018-10-06 20:51:24 -0400

    json 5 complete

---
+++ FreeCodeCamp/lighthouse/5.js
@@ -0,0 +1,19 @@
+function convertColumn(i) {
+    let x = i.split("");
+    let a = x[0];
+    switch (expression) {
+        case a = "A":
+            return 1
+            break;
+        case a = "B":
+            return 2
+            break;
+        case a = "C":
+            return 3
+            break;
+        default:
+            return x        
+    } */
+}
+
+console.log(convertColumn("C1"));
