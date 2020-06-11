fd952efb3d76c969e91e346e9f9ec8308f0f5863
chauxvive <mpauldesigns@gmail.com>
2020-06-11 16:22:15 -0400

    aw yes sum primes done mothertrucker

--- FreeCodeCamp/2.JavaScriptAlgorithmsStructures/SumPrimes.js
+++ FreeCodeCamp/2.JavaScriptAlgorithmsStructures/SumPrimes.js
@@ -1,27 +1,25 @@
 function sumPrimes(num) {
-    let primesList = [2];
+    let fullList = [2];
+    let primesList = [];
     let newPrime;
     let i = 2;
-    let x = 0;
+    let j = 0;
 
-    if (num==1){
-        return 1;
-    }
-    else if (num ==2){
-        return 2;
-    }
-
-    else if (num>2){
-        //let max = Math.sqrt(num);
-        while (i < num) {
-            while (x < primesList.length) {
-                if (i % primesList[x] != 0) {
-                    primesList.push(i);
-                }
-                x++;
-            }
+   if (num=>2){
+       while (i<=num){
+        fullList.push(i);
         i++;
         }
+
+        while (j<fullList.length){
+            primesList.push(fullList[0]);
+            fullList = fullList.filter(x => x % fullList[j] != 0);
+        }
+        
+
+   }
+    else if (num==1){
+        return 1;
     }
 
     else {return undefined;}
@@ -35,24 +33,21 @@ function sumPrimes(num) {
     //     primes.push(num);
     //     num--;
     // }
-    
-    
-    
-    console.log(primesList);
 
 
  let sum = primesList.reduce((a, b) => a + b, 0);
+ //console.log(primesList);
  console.log(sum);
  return sum;
 
 }
 
-sumPrimes(10);
+sumPrimes(20);
 //2,3,5,7
 
-//sumPrimes(10) 
+sumPrimes(10) 
 //should return a number.
-//sumPrimes(10) 
+sumPrimes(10) 
 //should return 17.
-//sumPrimes(977) 
+sumPrimes(977) 
 //should return 73156.
