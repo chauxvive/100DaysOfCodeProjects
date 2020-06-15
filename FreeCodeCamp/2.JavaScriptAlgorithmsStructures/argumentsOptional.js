a4c347877bac8e7e853c7d88345e5114b35d9c66
chauxvive <mpauldesigns@gmail.com>
2020-06-15 16:06:33 -0400

    started argumentsOptional

---
+++ FreeCodeCamp/2.JavaScriptAlgorithmsStructures/argumentsOptional.js
@@ -0,0 +1,33 @@
+function addTogether(a, b) {
+    if (Number.isInteger(a)===true&&Number.isInteger(b)===true){
+        console.log(a+b);
+        return a+b;
+    }
+    else if (addTogether.arguments.length>1&&(Number.isInteger(a)===false|Number.isInteger(b)===false)){
+        console.log(undefined);
+        return undefined;}
+    else if (addTogether.arguments.length==1&&Number.isInteger(a)===false){
+        console.log(undefined);
+        return undefined;}
+    else if (addTogether.arguments.length == 1&&Number.isInteger(a)===true){
+        console.log("b is missing.");
+    }
+}
+
+
+addTogether(2);    
+
+
+addTogether(2, 3);
+//should return 5.*
+
+addTogether(23, 30);
+//should return 53.
+//addTogether(5)(7);
+//should return 12.
+addTogether("http://bit.ly/IqT6zt"); 
+//should return undefined
+addTogether(2, "3") ;
+//should return undefined.
+//addTogether(2)([3]) ;
+//should return undefined.
