25ed369529fea10a14a2ebe77d983e1fd95be0c8
chauxvive <mpauldesigns@gmail.com>
2020-06-16 22:32:43 -0400

    arguments Optional complete

--- FreeCodeCamp/2.JavaScriptAlgorithmsStructures/argumentsOptional.js
+++ FreeCodeCamp/2.JavaScriptAlgorithmsStructures/argumentsOptional.js
@@ -1,48 +1,26 @@
 function addTogether(a, b) {
-
-   if (addTogether.arguments.length==1){
-        if (Number.isInteger(a)===true){
+    if (arguments.length === 1) {
+        if (Number.isInteger(a) === true) {
             return function (b) {
-                console.log(a + b);
-                return a + b;
-            };
-        }
-        else {
-            console.log(undefined);
-            return undefined;
+                if (Number.isInteger(b) === true) {
+                    return a + b;
+                } else {
+                    return undefined;
+                }
             }
+        } else {
+            return undefined;
         }
-    else {
-        if (Number.isInteger(a)===true&&Number.isInteger(b)===true){
-            console.log(a+b);
-            return a+b;
-        }
-        else {
-            console.log(undefined);
+    } else {
+        if (Number.isInteger(a) === true && (Number.isInteger(b) === true)) {
+            return a + b;
+        } else {
             return undefined;
         }
     }
+}
 
-   
-   
-   
-   
-   
-    // if (Number.isInteger(a)===true&&Number.isInteger(b)===true){
-    //     console.log(a+b);
-    //     return a+b;
-    // }
-    // else if (addTogether.arguments.length>1&&(Number.isInteger(a)===false|Number.isInteger(b)===false)){
-    //     console.log(undefined);
-    //     return undefined;}
-    // else if (addTogether.arguments.length==1&&Number.isInteger(a)===false){
-    //     console.log(undefined);
-    //     return undefined;}
-    // else if (addTogether.arguments.length == 1&&Number.isInteger(a)===true){
-    //     console.log("b is missing.");
-    // }
-
-
+addTogether(2, 3);
 }
 
 
